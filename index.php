e8638ec2c34b490e0a587119d4a5a3b261ed9c30
lasse-schmidt <l.s10600@gmail.com>
2022-09-30 08:56:44 +0200

    Default style.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Charset-->
+    <meta charset="UTF-8">
+
+    <!--Viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5">
+
+    <!--Title-->
+    <title>Oversætteren</title>
+
+    <!--Reset css, Style css, Starts css-->
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+    <!--Ajax lib-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!--Font-awesome-->
+    <script src="https://kit.fontawesome.com/7a26c8da44.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
