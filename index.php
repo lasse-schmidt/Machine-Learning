a2320e85a541b9c8e25e4e7edb25cc8232170c65
lasse-schmidt <l.s10600@gmail.com>
2022-10-05 10:10:58 +0200

    made site responsive.

--- index.php
+++ index.php
@@ -24,24 +24,70 @@
     <script src="https://kit.fontawesome.com/7a26c8da44.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <form>
+    <form onSubmit="return false;" id="translate">
       <div>
-        <select >
-          <option >Dansk</option>
+        <select name="input-lang" id="input-lang">
+	        <option value="auto" selected>Auto Detect</option>
+          <option value="ar">Arabisk</option>
+          <option value="da">Dansk</option>
+          <option value="en">Engelsk</option>
+          <option value="fi">Finsk</option>
+          <option value="fr">Fransk</option>
+          <option value="hi">Hindi</option>
+          <option value="nl">Hollandsk</option>
+          <option value="it">Italiensk</option>
+          <option value="ja">Japansk</option>
+          <option value="zh-CN">Kinesisk</option>
+          <option value="no">Norsk</option>
+          <option value="pl">Polsk</option>
+          <option value="pt">Portugisisk</option>
+          <option value="ru">Russisk</option>
+          <option value="es">Spansk</option>
+          <option value="sv">Svensk</option>
+          <option value="de">Tysk</option>
         </select>
 
-        <textarea  placeholder="Angiv tekst"></textarea>
+        <textarea  placeholder="Angiv tekst" id="source"></textarea>
+        <div class="options-empty">
+
+        </div>
       </div>
       <div>
-        <select >
-          <option >Dansk</option>
+        <select name="output-lang" id="output-lang">
+          <option value="ar">Arabisk</option>
+          <option value="da">Dansk</option>
+          <option value="en" selected>Engelsk</option>
+          <option value="fi">Finsk</option>
+          <option value="fr">Fransk</option>
+          <option value="hi">Hindi</option>
+          <option value="nl">Hollandsk</option>
+          <option value="it">Italiensk</option>
+          <option value="ja">Japansk</option>
+          <option value="zh-CN">Kinesisk</option>
+          <option value="no">Norsk</option>
+          <option value="pl">Polsk</option>
+          <option value="pt">Portugisisk</option>
+          <option value="ru">Russisk</option>
+          <option value="es">Spansk</option>
+          <option value="sv">Svensk</option>
+          <option value="de">Tysk</option>
         </select>
 
-        <div>
-          <textarea  placeholder="Oversættelse"></textarea>
-          <span class="fas fa-volume-up tts-icon"></span>
+        <textarea placeholder="Oversættelse" readonly id="target"></textarea>
+
+        <div class="options">
+          <span class="far fa-copy copy-icon" title="Kopier tekst" onclick="copyText();"></span>
+          <span class="fas fa-volume-up tts-icon" title="Oplæs tekst" onclick="play();"></span>
         </div>
       </div>
     </form>
+
+    <div id="audio">
+
+    </div>
+
+    <script src="js/translate.js"></script>
+    <script src="js/copytext.js"></script>
+    <script src="js/tts.js"></script>
   </body>
 </html>
